18870f3b455ef9d330763e55278034eef59540c1
MattM <67703490+Matthew-Muir@users.noreply.github.com>
2021-11-19 00:12:02 +0000

    Add patient form functional

--- script.js
+++ script.js
@@ -7,20 +7,62 @@ class Patient {
 }
 
 class ERQueue {
-  queue = [[],[],[],[],[]]; //an array for each priority of patient 1-5
+  queue = [[], [], [], [], []]; //an array for each priority of patient 1-5
 
   enqueue(patient) {
     this.queue[patient.priority - 1].push(patient);
   }
 
   dequeue() {
-    
+    let patientAtFrontOfLine;
+
+    for (let i = 4; i >= 0; i--) {
+      if (this.queue[i].length > 0) {
+        patientAtFrontOfLine = this.queue[i].shift();
+        return patientAtFrontOfLine;
+      }
+    }
+    return "Queue Empty";
   }
 
+  outPatientList() {
+    console.log(this.queue.flat());
+  }
 }
 
-function clicked() {
+const enqueue = new ERQueue();
+
+
+
+
+function submitButtonClicked() {
+  //link to HTML form elements
   const htmlElement = document.getElementById("data");
-  counter++;
-  htmlElement.innerHTML = counter;
+  const firstNameField = document.getElementById("fname");
+  const lastNameField = document.getElementById("lname");
+  const priority = document.getElementById("priority");
+
+  //Validation check that form is fully filled out
+  if (firstNameField.value == '' || lastNameField.value == '') {
+    htmlElement.innerHTML = "Please Fill Out All Fields";
+  }
+
+  //form is filled out
+  else {
+    //create patient and add to que
+    const newPatient = new Patient(firstNameField.value, lastNameField.value, priority.value);
+    enqueue.enqueue(newPatient);
+
+    //output text to window for user to know patient added
+    let innerHTML = `Added Priorty(${priority.value}) Patient: ${firstNameField.value} ${lastNameField.value}`;
+    htmlElement.outerHTML = `<h4>${innerHTML}</h4>`;
+
+
+    //clear form fields
+    firstNameField.value = '';
+    lastNameField.value = '';
+    priority.value = 1;
+  }
+
+
 }
